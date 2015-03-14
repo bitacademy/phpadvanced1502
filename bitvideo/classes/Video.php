52142a01cbc7ae5cd0022ab1c6ada1a1eb5f3425
Popa Vlad Cristian <vladpopa0713@gmail.com>
2015-03-15 00:27:20 +0200

    Update Video.php
    Adaugat 3 functii : insert, update, save si am extins clasa Video

--- bitvideo/classes/Video.php
+++ bitvideo/classes/Video.php
@@ -1,13 +1,50 @@
 <?php
 
-class Video {
+class Video extends BaseDB {
     
     private $id;
     private $url;
     private $id_pachet;
     private $data_publicare;
     private $status;
-
+    
+    
+     public function insert() {
+        $db = $this->db;
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO video (url, id_pachet, data_publicare, status) values(?, ?, ?, ?)";
+        $q = $db->prepare($sql);
+        $q->execute(array(
+            $this->getUrl(),
+            $this->getId_pachet(),
+            $this->getData_publicare(),
+            $this->getStatus()
+        ));
+    }
+    
+    
+    public function update() {
+        $db = $this->db;
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "UPDATE video  set url = ?, id_pachet = ?, data_publicare = ?, status = ? WHERE id = ?";
+        $q = $db->prepare($sql);
+        $q->execute(array(
+            $this->getUrl(),
+            $this->getId_pachet(),
+            $this->getData_publicare(),
+            $this->getStatus(),
+            $this->getId()
+        ));
+    }
+    
+    
+    public function save() {
+        if($this->getId = true){
+            $this->update();
+        }else {
+            $this->insert();
+        }
+    }
 
     public function setId($id) {
         $this->id = $id;
