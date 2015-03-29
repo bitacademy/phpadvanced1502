59f9ff037c3cb3b29907f9c91a33c8e29a2c4682
climaxx <suport@brunomag.ro>
2015-03-29 12:07:09 +0300

    adaugat logica de cod pentru aplicatia bitvideo - daca utilizatorul este logat si a platit pachetul curent, afiseaza-i informatia, altfel redirecteaza-l la login si la upgrade

--- bitvideo/frontend/header.php
+++ bitvideo/frontend/header.php
@@ -1,10 +1,55 @@
 <?php
+require_once "../autoload.php";
+
+try{
+
+
+$paginaCurentaArrayTipSiId = Pagina::getPaginaCurenta();
+$paginaCurentaArrayTipSiId['tip_pagina'] = 'pagina';
+//$paginaCurentaArrayTipSiId['tip_pagina'] = 'video';
+//$paginaCurentaArrayTipSiId['id'] = 1;
+if ($paginaCurentaArrayTipSiId['tip_pagina'] == 'video') {
+    $idVideo = $paginaCurentaArrayTipSiId['id'];
+    $tipPagina = 'video';
+}
+
+else if ($paginaCurentaArrayTipSiId['tip_pagina'] == 'pagina') {
+    $idVideo = false;
+    $tipPagina = 'pagina';
+}
+else {
+//eroare in arrayul $paginaCurentaArrayTipSiId
+    $idVideo = null;
+}
+
+ if($customerDataSerializedIfLoggedIn = Customer::isLoggedIn() ) {
+    //daca este logat, verificam daca are acces la pagina curenta
+    $customerData = unserialize($customerDataSerializedIfLoggedIn);
+    $pdo = Database::connect();
+    $user = BaseDB::fill($customerData, 'Customer', $pdo );
+     if ($idVideo === null ||   !$user->hasAccess($idVideo, $tipPagina)) {
+         header("Location: error.php?message=upgrade");
+         exit();
+     }
+     else {
+         //permitem incarcarea paginii daca a fost setat $idVideo
+         //la care are acces sau daca daca este o pagina
+
+     }
+ }
+
+//daca nu este logat, redirect la login
+else {
+    header("Location: login.php");
+    exit();
+}
+}
+catch(Exception $exc){
+    echo "<PRE>A aparut o eroare:";
+    print_r($exc);
+    echo "</PRE>";
+}
+require_once "header_html.php";
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <link   href="../admin/css/bootstrap.min.css" rel="stylesheet">
-    <link   href="../admin/css/signin.css" rel="stylesheet">
-</head>
-<body>
+
