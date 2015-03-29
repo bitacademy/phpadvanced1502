0034d529880e29bf577d30188560895d7c0b286c
Serpele <serpele_2005@yahoo.com>
2015-03-29 15:29:25 +0300

    Am adaugat videos.php in frontent

--- bitvideo/frontend/videos.php
+++ bitvideo/frontend/videos.php
@@ -1,2 +1,18 @@
 <?php
-require_once "header.php";
+require_once "../autoload.php";
+$pdo = Database::connect();
+$videos = Video::showAll($pdo, 'video');
+$videogasit = false;
+foreach($videos as $video){
+    if($_GET['id']==$video->getID()){
+        echo $video->getId();
+        echo "<br/> <br/>";
+        $youtubeUrl = $video->parseYoutubeUrlForEmbed ( $video->getUrl());
+        echo '<iframe width="420" height="315"
+            src="'.   $youtubeUrl .  '"
+            frameborder="0" allowfullscreen></iframe>';
+    $videogasit = true;
+    }   
+        
+}
+if(!$videogasit) echo "Nu exista video-ul";
