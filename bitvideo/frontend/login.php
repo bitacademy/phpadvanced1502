b57c0ae7b7b7f027af313b98a208946b7f02d508
climaxx <suport@brunomag.ro>
2015-03-29 12:06:21 +0300

    adaugat rutina de login in login.php in functie de

--- bitvideo/frontend/login.php
+++ bitvideo/frontend/login.php
@@ -1,21 +1,39 @@
 <?php
-require ('header.php');
+require_once "../autoload.php";
+
+if (isset($_POST['submit_button'])){
+    $errorMessage = '';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $pdo = Database::connect();
+    $c = new Customer($pdo);
+    $status = $c->doLogin($username, $password);
+    if ($status) {
+        header("Location: index.php");
+        exit();
+    }
+    else {
+        $errorMessage = "Datele de conectare sunt incorecte";
+    }
+}
+
+
+
+
+require_once "header_html.php";
 ?>
 
 <div class="container">
 
-      <form class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
+      <form class="form-signin" method="post" action="">
+        <?php if (isset($errorMessage)) echo $errorMessage; ?>
+        <h1>BITVIDEO </h1>
+        <h2 class="form-signin-heading"> Please sign in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input name="username" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button name="submit_button" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
     </div> <!-- /container -->
