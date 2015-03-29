004227cb5e6b9cb2bfa8566edc2df8c14487cd30
climaxx <suport@brunomag.ro>
2015-03-29 12:04:44 +0300

    actualizat caile catre CSS / JS in header si in footer

--- bitvideo/frontend/footer.php
+++ bitvideo/frontend/footer.php
@@ -1,5 +1,6 @@
 <?php
 ?>
-  <script src="../admin/js/bootstrap.min.js"></script>
+  <script src="js/jquery-1.11.2.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ bitvideo/frontend/header_html.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <link   href="css/signin.css" rel="stylesheet">
+    <link   href="css/justified-nav.css" rel="stylesheet">
+</head>
+<body>
